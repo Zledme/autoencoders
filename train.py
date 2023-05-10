3e5135fc4403f195319f66fdc6e88ce864e3c4c7
ridu <zledme@tutanota.com>
2023-05-10 08:13:04 +0530

    try4

--- train.py
+++ train.py
@@ -6,7 +6,38 @@ import torch.optim as optim
 import torchvision
 from torchvision import transforms
 
-def train(model, optimizer, loss_fn):
+
+def train_autoencoder():
+    # Define a transform to convert PIL images to tensors and normalize them
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5,), (0.5,)),
+        transforms.Lambda(lambda x: torch.reshape(x, (1,)))
+        ])
+    # Download and load the training data
+    trainset = torchvision.datasets.MNIST(root='./data', train=True,
+                                            download=True, transform=transform)
+
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                              shuffle=True, num_workers=2)
+
+    # Download and load the test data
+    testset = torchvision.datasets.MNIST(root='./data', train=False,
+                                        download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                            shuffle=False, num_workers=2)
+    optimizer = optim.Adam(autoencoder.parameters(), lr=lr)
+    loss_fn = nn.MSELoss()
+
+
+    #init the model
+    model = Autoencoders(
+            input_shape=(1, 28, 28),
+            conv_filters=(32, 64, 64, 64),
+            conv_kernels=(3, 3, 3, 3),
+            conv_strides=(1, 2, 2, 1),
+            latent_space_dim=2
+        )
 
     batch_size = 32
     epochs = 20
@@ -38,40 +69,6 @@ def train(model, optimizer, loss_fn):
                 running_loss = 0.
 
         print(last_loss)
-    return model
-
-def train_autoencoder():
-    # Define a transform to convert PIL images to tensors and normalize them
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,)),
-        transforms.Lambda(lambda x: torch.reshape(x, (1,)))
-        ])
-    # Download and load the training data
-    trainset = torchvision.datasets.MNIST(root='./data', train=True,
-                                            download=True, transform=transform)
-
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                              shuffle=True, num_workers=2)
-
-    # Download and load the test data
-    testset = torchvision.datasets.MNIST(root='./data', train=False,
-                                        download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=4,
-                                            shuffle=False, num_workers=2)
-    optimizer = optim.Adam(autoencoder.parameters(), lr=lr)
-    loss_fn = nn.MSELoss()
-
-
-    #init the model
-    autoencoder = Autoencoders(
-            input_shape=(1, 28, 28),
-            conv_filters=(32, 64, 64, 64),
-            conv_kernels=(3, 3, 3, 3),
-            conv_strides=(1, 2, 2, 1),
-            latent_space_dim=2
-        )
-    model = train(autoencoder, optimizer, loss_fn)
     return model
 
 
